9ee022cec5179c719a7fcb8dc27d988fced45ccf
kumar804 <kumarsinghmanish547@gmail.com>
2021-08-23 16:26:24 +0530

    Added: Welcome  Message

---
+++ main/java/AddressBookController/AddressBookSystem.java
@@ -0,0 +1,7 @@
+package AddressBookController;
+
+public class AddressBookSystem {
+    public static void main(String[] args) {
+        System.out.println("Welcome to AddressBook System");
+    }
+}
