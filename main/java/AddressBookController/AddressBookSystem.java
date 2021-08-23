877524dff87ff9f72e0424791cc597b3f9b53254
kumar804 <kumarsinghmanish547@gmail.com>
2021-08-23 16:36:01 +0530

    Added Refactor code to add contact list

--- main/java/AddressBookController/AddressBookSystem.java
+++ main/java/AddressBookController/AddressBookSystem.java
@@ -1,7 +1,43 @@
 package AddressBookController;
 
 public class AddressBookSystem {
-    public static void main(String[] args) {
-        System.out.println("Welcome to AddressBook System");
+    String first_name;
+    String last_name;
+    String address;
+    String city;
+    String state;
+    int zip;
+    String phone_number;
+    String email;
+    //Create a parameterized constructor
+    AddressBookSystem(String first_name, String last_name, String address, String city, String state, int zip, String phone_number, String email) {
+        this.first_name = first_name;
+        this.last_name = last_name;
+        this.address = address;
+        this.city = city;
+        this.state = state;
+        this.zip = zip;
+        this.phone_number = phone_number;
+        this.email = email;
     }
-}
+    public static void main(String args[]){
+        //Create object and pass the values
+        AddressBookSystem obj = new AddressBookSystem("SAYANI","KOLEY","Howrah", "Howrah", "West Bengal", 711101, "7908", "sayanikoley8@gmail.com");
+
+        //Display the values of object
+        obj.display();
+    }
+    public void display() {
+        System.out.println("First Name: " +first_name);
+        System.out.println("Last Name: " +last_name);
+        System.out.println("Address: " +address);
+        System.out.println("City: " +city);
+        System.out.println("State: " +state);
+        System.out.println("Zip: " +zip);
+        System.out.println("Phone number: " +phone_number);
+        System.out.println("Email: " +email);
+    }
+
+
+    }
+
