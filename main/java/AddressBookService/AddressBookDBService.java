f4cfa5f39ff9b47801589f232aed457628ccefbe
kumar804 <kumarsinghmanish547@gmail.com>
2021-08-24 10:04:03 +0530

    Added : refactor code to retrieve contact from database

--- main/java/AddressBookService/AddressBookDBService.java
+++ main/java/AddressBookService/AddressBookDBService.java
@@ -3,11 +3,14 @@ package AddressBookService;
 import AddressBookModel.PersonInfo;
 
 import java.sql.*;
+import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
 
 public class AddressBookDBService {
     private static AddressBookDBService addressBookDBService;
+    private PreparedStatement personInfoDataStatement;
+
     private AddressBookDBService() {
     }
 
@@ -33,7 +36,7 @@ public class AddressBookDBService {
     private Connection getConnection() throws SQLException {
         String jdbcURL = "jdbc:mysql://localhost:3306/addressbookdb?useSSL=false";
         String userName = "root";
-        String password = "manish";
+        String password = "root";
         Connection connection;
         System.out.println("Connecting to database: "+jdbcURL);
         connection = DriverManager.getConnection(jdbcURL, userName, password);
@@ -49,7 +52,7 @@ public class AddressBookDBService {
      * @return
      */
 
-    private List<PersonInfo> getEmployeePayrollDataUsingDB(String sql) {
+    private List<PersonInfo> getPersonInfoDataUsingDB(String sql) {
         List<PersonInfo> personInfoList = new ArrayList<>();
         try (Connection connection = this.getConnection()) {
             Statement statement = connection.createStatement();
@@ -80,6 +83,116 @@ public class AddressBookDBService {
 
     public List<PersonInfo> readData() {
         String sql = "SELECT * FROM address_book_records";
-        return getEmployeePayrollDataUsingDB(sql);
+        return getPersonInfoDataUsingDB(sql);
+    }
+
+    /**
+     * Purpose : Update the state in the DB using Statement Interface
+     *
+     * @param name
+     * @param state
+     * @return
+     */
+
+    public int updatePersonInfo(String name, String state) {
+        return this.updatePersonInfoUsingStatement(name,state);
+    }
+
+    /**
+     * Purpose : Update the state in the DB using Statement Interface
+     *
+     * @param name
+     * @param state
+     * @return
+     */
+
+    private int updatePersonInfoUsingStatement(String name, String state) {
+        String sql = String.format("UPDATE address_book_records SET State = '%s' WHERE FirstName = '%s';", state, name);
+        try (Connection connection = this.getConnection()){
+            Statement statement = connection.createStatement();
+            return statement.executeUpdate(sql);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return 0;
+    }
+
+    /**
+     * Purpose : Get the list of PersonInfoData using the assigned name
+     *           setString() is used to set the assigned name value in the sql query
+     *           Return all the attribute values listed for a particular name
+     *
+     * @param name
+     * @return
+     */
+
+    public List<PersonInfo> getPersonInfoData(String name) {
+        List<PersonInfo> personInfoList = null;
+        if(this.personInfoDataStatement == null)
+            this.preparedStatementForPersonInfo();
+        try {
+            personInfoDataStatement.setString(1, name);
+            ResultSet resultSet = personInfoDataStatement.executeQuery();
+            personInfoList = this.getPersonInfoData(resultSet);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return personInfoList;
+    }
+
+    /**
+     * Purpose : Assign the value of the attributes in a list and return it
+     *
+     * @param resultSet
+     * @return
+     */
+
+    private List<PersonInfo> getPersonInfoData(ResultSet resultSet) {
+        List<PersonInfo> personInfoList = new ArrayList<>();
+        try {
+            while (resultSet.next()) {
+                int id = resultSet.getInt("A_ID");
+                String fname = resultSet.getString("FirstName");
+                String lname = resultSet.getString("LastName");
+                String address = resultSet.getString("Address");
+                String city = resultSet.getString("City");
+                String state = resultSet.getString("State");
+                int zip = resultSet.getInt("Zip");
+                String phoneNo = resultSet.getString("PhoneNumber");
+                String email = resultSet.getString("Email");
+                personInfoList.add(new PersonInfo(id, fname, lname, address, city, state, zip, phoneNo, email));
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return personInfoList;
+    }
+
+    /**
+     * Purpose : To get the details of a particular person from the DB using PreparedStatement Interface
+     */
+
+    private void preparedStatementForPersonInfo() {
+        try {
+            Connection connection = this.getConnection();
+            String sql = "SELECT * FROM address_book_records WHERE FirstName = ?";
+            personInfoDataStatement = connection.prepareStatement(sql);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     * Purpose : Read the data for a certain date range from the database
+     *
+     * @param startDate
+     * @param endDate
+     * @return
+     */
+
+    public List<PersonInfo> getPersonInfoData(LocalDate startDate, LocalDate endDate) {
+        String sql = String.format("SELECT * FROM address_book_records WHERE Date__Added BETWEEN '%s' AND '%s';",
+                Date.valueOf(startDate), Date.valueOf(endDate));
+        return getPersonInfoDataUsingDB(sql);
     }
 }
--- main/java/AddressBookService/AddressBookService.java
+++ main/java/AddressBookService/AddressBookService.java
@@ -2,6 +2,7 @@ package AddressBookService;
 
 import AddressBookModel.PersonInfo;
 
+import java.time.LocalDate;
 import java.util.List;
 
 public class AddressBookService {
@@ -27,5 +28,67 @@ public class AddressBookService {
         if(ioService.equals(IOService.DB_IO))
             this.personInfoList = addressBookDBService.readData();
         return this.personInfoList;
+    }
+
+    /**
+     * Purpose : To update the Person Info State in the database
+     *           If the value is updated, the result value returned is greater than 0; else it is returned 0
+     *           Match the given name with the PersonInfo list
+     *           If found, assign the given state to the PersonInfo list
+     *
+     * @param name
+     * @param state
+     */
+
+    public void updatePersonInfo(String name, String state) {
+        int result = addressBookDBService.updatePersonInfo(name, state);
+        if(result == 0)
+            return;
+        PersonInfo personInfoData = this.getPersonInfoData(name);
+        if( personInfoData != null )
+            personInfoData.state = state;
+    }
+
+    /**
+     * Purpose : Check the PersonInfo list for the name
+     *           If found, return the value else return null
+     *
+     * @param name
+     * @return
+     */
+
+    private PersonInfo getPersonInfoData(String name) {
+        return this.personInfoList.stream()
+                .filter(personInfo -> personInfo.first_name.equals(name))
+                .findFirst()
+                .orElse(null);
+    }
+
+    /**
+     * Purpose : To check whether the PersonInfo is in sync with the DB
+     *           Use to equals() to compare the values
+     *
+     * @param name
+     * @return
+     */
+
+    public boolean checkPersonInfoInSyncWithDB(String name) {
+        List<PersonInfo> personInfoDataList = addressBookDBService.getPersonInfoData(name);
+        return personInfoDataList.get(0).equals(getPersonInfoData(name));
+    }
+
+    /**
+     * Purpose : Retrieve the data for a particular date range
+     *
+     * @param ioService
+     * @param startDate
+     * @param endDate
+     * @return
+     */
+
+    public List<PersonInfo> readPersonInfoForDateRange(IOService ioService, LocalDate startDate, LocalDate endDate) {
+        if( ioService.equals(IOService.DB_IO) )
+            return addressBookDBService.getPersonInfoData(startDate, endDate);
+        return null;
     }
 }
