4fdba6f27d2ee7ca27ee9602542aa2a8a82b6021
kumar804 <kumarsinghmanish547@gmail.com>
2021-08-24 09:47:54 +0530

    Added Refactor code to connect addressbook data base using jdbc

--- main/java/AddressBookModel/PersonInfo.java
+++ main/java/AddressBookModel/PersonInfo.java
@@ -1,104 +1,113 @@
 package AddressBookModel;
 
+
 public class PersonInfo {
-    String first_name;
-    String last_name;
-    String address;
-    String city;
-    String state;
-    int zip;
-    String phone_number;
-    String email;
 
-    public PersonInfo() {
-    }
+        int id;
+        String first_name;
+        String last_name;
+        String address;
+        String city;
+        String state;
+        int zip;
+        String phone_number;
+        String email;
 
-    @Override
-    public String toString() {
-        return "PersonInfo{" +
-                "first_name='" + first_name + '\'' +
-                ", last_name='" + last_name + '\'' +
-                ", address='" + address + '\'' +
-                ", city='" + city + '\'' +
-                ", state='" + state + '\'' +
-                ", zip=" + zip +
-                ", phone_number='" + phone_number + '\'' +
-                ", email='" + email + '\'' +
-                '}';
-    }
+        public PersonInfo() {
+        }
 
-    public PersonInfo(String first_name, String last_name, String address, String city, String state, int zip, String phone_number, String email) {
-        this.first_name = first_name;
-        this.last_name = last_name;
-        this.address = address;
-        this.city = city;
-        this.state = state;
-        this.zip = zip;
-        this.phone_number = phone_number;
-        this.email = email;
-    }
+        public PersonInfo(String first_name, String last_name, String address, String city, String state, int zip, String phone_number, String email) {
+            this.first_name = first_name;
+            this.last_name = last_name;
+            this.address = address;
+            this.city = city;
+            this.state = state;
+            this.zip = zip;
+            this.phone_number = phone_number;
+            this.email = email;
+        }
 
-    public String getFirst_name() {
-        return first_name;
-    }
+        public PersonInfo(int id,String first_name, String last_name, String address, String city, String state, int zip, String phone_number, String email) {
+            this(first_name, last_name, address, city, state, zip, phone_number, email);
+            this.id = id;
+        }
 
-    public void setFirst_name(String first_name) {
-        this.first_name = first_name;
-    }
+        public String getFirst_name() {
+            return first_name;
+        }
 
-    public String getLast_name() {
-        return last_name;
-    }
+        public void setFirst_name(String first_name) {
+            this.first_name = first_name;
+        }
 
-    public void setLast_name(String last_name) {
-        this.last_name = last_name;
-    }
+        public String getLast_name() {
+            return last_name;
+        }
 
-    public String getAddress() {
-        return address;
-    }
+        public void setLast_name(String last_name) {
+            this.last_name = last_name;
+        }
 
-    public void setAddress(String address) {
-        this.address = address;
-    }
+        public String getAddress() {
+            return address;
+        }
 
-    public String getCity() {
-        return city;
-    }
+        public void setAddress(String address) {
+            this.address = address;
+        }
 
-    public void setCity(String city) {
-        this.city = city;
-    }
+        public String getCity() {
+            return city;
+        }
 
-    public String getState() {
-        return state;
-    }
+        public void setCity(String city) {
+            this.city = city;
+        }
 
-    public void setState(String state) {
-        this.state = state;
-    }
+        public String getState() {
+            return state;
+        }
 
-    public int getZip() {
-        return zip;
-    }
+        public void setState(String state) {
+            this.state = state;
+        }
 
-    public void setZip(int zip) {
-        this.zip = zip;
-    }
+        public int getZip() {
+            return zip;
+        }
 
-    public String getPhone_number() {
-        return phone_number;
-    }
+        public void setZip(int zip) {
+            this.zip = zip;
+        }
 
-    public void setPhone_number(String phone_number) {
-        this.phone_number = phone_number;
-    }
+        public String getPhone_number() {
+            return phone_number;
+        }
 
-    public String getEmail() {
-        return email;
-    }
+        public void setPhone_number(String phone_number) {
+            this.phone_number = phone_number;
+        }
 
-    public void setEmail(String email) {
-        this.email = email;
+        public String getEmail() {
+            return email;
+        }
+
+        public void setEmail(String email) {
+            this.email = email;
+        }
+
+        @Override
+        public String toString() {
+            return "PersonInfo{" +
+                    "first_name='" + first_name + '\'' +
+                    ", last_name='" + last_name + '\'' +
+                    ", address='" + address + '\'' +
+                    ", city='" + city + '\'' +
+                    ", state='" + state + '\'' +
+                    ", zip=" + zip +
+                    ", phone_number='" + phone_number + '\'' +
+                    ", email='" + email + '\'' +
+                    '}';
+        }
+
     }
-}
